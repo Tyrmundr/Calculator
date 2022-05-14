2422b763746333f6740a24693de4f2f2aa9fb60b
Tyrmundr <taki.96061@gmail.com>
2022-05-14 16:38:18 +0200

    Add base JS, start working on methods

---
+++ script.js
@@ -0,0 +1,36 @@
+"use strict"
+
+const calculator = {
+    previous: null,
+    current: null,
+    operator: null,
+
+    /*
+    delete() {
+        //code
+    },
+    */
+
+    /*
+    compute() {
+        let result;
+        switch(this.operator) {
+            case "+":
+                result = this.previous + this.current;
+                break;
+            case "-":
+                result = this.previous - this.current;
+                break;
+            case "/":
+                result = this.previous / this.current;
+                break;
+            case "*":
+                result = this.previous * this.current;
+                break;
+            default:
+                alert("No operator was chosen, cannot calculate!");
+        }
+        return result;
+    }
+    */
+}
