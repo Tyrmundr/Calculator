752d556ab0b8697c61bab294dd062020d4155195
Tyrmundr <taki.96061@gmail.com>
2022-05-15 15:25:00 +0200

    Add append and operate methods for adding numbers and operators

--- script.js
+++ script.js
@@ -1,9 +1,38 @@
 "use strict"
 
+//DOM
+const btnEquals = document.querySelector(".btn-equals");
+const btnDelete =document.querySelector(".btn-delete");
+const btnClear = document.querySelector(".btn-clear");
+const numbers = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".operator");
+
+const previousTextElement = document.querySelector(".previous");
+const currentTextElement = document.querySelector(".current");
+
+//Object instance
 const calculator = {
-    previous: null,
-    current: null,
-    operator: null,
+    current: "",
+    previous: "",
+    operator: "",
+
+    append(element) {
+        if(element.target.innerText === "." && calculator.current.includes(".")) return;
+        
+        calculator.current += element.target.innerText;
+
+        updateUI();
+    },
+
+    operate(element) {
+        if(calculator.current === "") return;
+
+        calculator.previous = calculator.current;
+        calculator.current = "";
+        calculator.operator = element.target.innerText;
+
+        updateUI();
+    }
 
     /*
     delete() {
@@ -34,3 +63,15 @@ const calculator = {
     }
     */
 }
+
+//Non-object functions
+
+function updateUI() {
+    currentTextElement.innerText = calculator.current;
+    previousTextElement.innerText = `${calculator.previous} ${calculator.operator}`;
+}
+
+//Event listeners
+numbers.forEach(number => number.addEventListener("click", calculator.append));
+
+operators.forEach(operator => operator.addEventListener("click", calculator.operate));
